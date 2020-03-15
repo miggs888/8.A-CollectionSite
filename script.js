25a4396f73f553e1fa77d402aab86b96910871d6
Lauren Miggins <lauren.miggins@gmail.com>
2020-03-15 19:23:29 -0400

    additing inital files from previous assignment -7.A

---
+++ script.js
@@ -0,0 +1,50 @@
+var mySaucesArray = [
+  {
+    name: "Tapatio",
+    variation: "Original",
+    heat: "4 of 5",
+    bottle: "5 oz",
+    manufacturer: "Tapatio Foods INC",
+    location: "Vernon, CA"
+  },
+  {
+    name: "Mike\'s Hot Honey",
+    variation: "Original",
+    heat: "3 of 5",
+    bottle: "12 oz",
+    manufacturer: "Mike\'s Hot Honey INC",
+    location: "Brooklyn, NY"
+  },
+  {
+    name: "Texas Pete",
+    variation: "Original",
+    heat: "3 of 5",
+    bottle: "12 oz",
+    manufacturer: "TW Garner Food INC",
+    location: "Winston-Salem, NC"
+  },
+  {
+    name: "Frank\'s Red Hot",
+    variation: "Buffalo",
+    heat: "4 of 5",
+    bottle: "12 oz",
+    manufacturer: "French\'s Food Company LLC",
+    location: "Springfield, MO"
+  },
+  {
+    name: "Cholula",
+    variation: "Chipotle",
+    heat: "4 of 5",
+    bottle: "5 oz",
+    manufacturer: "Cholula Food Company",
+    location: "Mexico"
+  },
+  
+];
+
+var vm = new Vue({
+  el:"#app",
+  data: {
+    sauceList: mySaucesArray
+  }
+});
